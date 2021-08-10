229812407d96e2605798472bacb26be2a4a50253
KingMabuza <amal@amallevi.com>
2021-08-10 19:53:51 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template,abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+import sqlalchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@localhost/trial"
+app.config['SECRET_KEY'] = 'thisisthesecretkeyforthisproject'
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    subtitle = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime)
+    slug = db.Column(db.String(255))
+
+
+admin.add_view(ModelView(Posts, db.session))
+
+
+@app.route('/')
+def home():
+    posts = Posts.query.all()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/post/<string:slug>')
+def post(slug):
+    try:
+        post = Posts.query.filter_by(slug=slug).one()
+        return render_template('post.html', post=post)
+    except sqlalchemy.orm.exc.NoResultFound:
+        abort(404)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run()
